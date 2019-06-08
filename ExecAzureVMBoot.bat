2bc498f6efe88b16381c8650bf376243280f6d05
NaruhiroIkeya <51503619+NaruhiroIkeya@users.noreply.github.com>
2019-06-08 16:14:04 +0900

    Add files via upload

---
+++ ExecAzureVMBoot.bat
@@ -0,0 +1,91 @@
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Copyright(c) 2019 BeeX Inc. All rights reserved.
+:: @auther:Naruhiro Ikeya
+::
+:: @name:ExecAzureVMBoot.bat
+:: @summary:ExecAzureVMBoot.ps1 Wrapper
+::
+:: @since:2019/01/17
+:: @version:1.0
+:: @see:
+:: @parameter
+::  1:ResourceGroup名
+::  2:AzureVM名
+::
+:: @return:0:Success 1:Error
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+:::::::::::::::::::::::::::::
+::      環境変数設定       ::
+:::::::::::::::::::::::::::::
+SET __LOG_CYCLE__=7
+
+:::::::::::::::::::::::::::::::::::
+::      パラメータチェック       ::
+:::::::::::::::::::::::::::::::::::
+SET __ARGC__=0
+FOR %%a IN ( %* ) DO SET /A __ARGC__+=1
+
+IF %__ARGC__% neq 2 (
+  SET __TIME__=%TIME:~0,8%
+  SET __TIME__=!__TIME__: =0!
+  ECHO [%DATE% !__TIME__!] Usage:%~n0 ResourceGroup名 AzureVM名
+  EXIT /B 1
+) 
+
+SET __RESOURCEGROUPNAME__=%1
+SET __VMNAME__=%2
+
+::::::::::::::::::::::::::::::::::
+::      タイムスタンプ生成      ::
+::::::::::::::::::::::::::::::::::
+SET __TODAY__=%DATE:/=%
+SET __TIME__=%TIME::=%
+SET __TIME__=%__TIME__:.=%
+SET __NOW__=%__TODAY__%%__TIME__: =0%
+
+::::::::::::::::::::::::::::::::::::
+::      出力ログファイル生成      ::
+::::::::::::::::::::::::::::::::::::
+FOR /F "usebackq" %%L IN (`powershell -command "Split-Path %~dp0 -Parent | Join-Path -ChildPath log"`) DO SET __LOGPATH__=%%L
+IF NOT EXIST %__LOGPATH__% MKDIR %__LOGPATH__% 
+SET __LOGFILE__=%__LOGPATH__%\%~n0_%__NOW__%.log
+
+::::::::::::::::::::::::::::::::::::::::::::::
+::      出力ログファイルローテーション      ::
+::::::::::::::::::::::::::::::::::::::::::::::
+FORFILES /P %__LOGPATH__% /M *.log /D -%__LOG_CYCLE__% /C "CMD /C IF @isdir==FALSE DEL /Q @path" > NUL 2>&1
+
+::::::::::::::::::::::::::::::::::::::
+::      スクリプト本体存在確認      ::
+::::::::::::::::::::::::::::::::::::::
+IF NOT EXIST %~dpn0.ps1 (
+  CALL :__ECHO__ AzureVM起動スクリプトが存在しません。
+  EXIT /B 1
+)
+
+::::::::::::::::::::::::::::::::::
+::      スクリプト本体実行      ::
+::::::::::::::::::::::::::::::::::
+CALL :__ECHO__ AzureVM起動処理（%~n0.ps1）を開始します。
+powershell -NoProfile -inputformat none -command "%~dpn0.ps1 %__RESOURCEGROUPNAME__% %__VMNAME__%;exit $LASTEXITCODE" >>"%__LOGFILE__%"
+
+::::::::::::::::::::::::::::::::::::::::::
+::      スクリプト本体実行結果確認      ::
+::::::::::::::::::::::::::::::::::::::::::
+IF ERRORLEVEL 1 (
+  CALL :__ECHO__ AzureVM起動処理中にエラーが発生しました。
+  EXIT /B 1
+)
+CALL :__ECHO__ AzureVM起動処理が完了しました。
+
+:__QUIT__
+EXIT /B 0
+
+:__ECHO__
+SET __TIME__=%TIME:~0,8%
+ECHO [%DATE% %__TIME__: =0%] %*
+ECHO [%DATE% %__TIME__: =0%] %* >>"%__LOGFILE__%"
+EXIT /B 0
---
+++ RecoveryDataDisk.ps1
@@ -0,0 +1,228 @@
+<################################################################################
+## Copyright(c) 2019 BeeX Inc. All rights reserved.
+## @auther#Naruhiro Ikeya
+##
+## @name:RecoveryDataDisk.ps1
+## @summary:管理ディスクのスナップショットからデータディスクの復元
+##
+## @since:2019/02/03
+## @version:1.0
+## @see:
+## @parameter
+##  1:Azure VM名
+##  2:Azure VMリソースグループ名
+##
+## @return:0:Success 9:エラー終了
+################################################################################>
+
+##########################
+# パラメータ設定
+##########################
+param (
+  [parameter(mandatory=$true)][string]$AzureVMName,
+  [parameter(mandatory=$true)][string]$AzureVMResourceGroupName
+)
+
+##########################
+# 認証情報設定
+##########################
+#####################
+$TennantID = "2ab73ef2-d066-4ce0-923e-94235755e2a2"
+$TennantID = "e2fb1fde-e67c-4a07-8478-5ab2b9a0577f"
+$Key="AgndRfEIsRJ+8VjN0oQjy5T+vfnlcIQUUuYsXj780FM="
+$ApplicationID="ea70cdb1-df24-4928-9bf4-4ff6b6963463"
+
+##########################
+# 警告の表示抑止
+##########################
+Set-Item Env:\SuppressAzurePowerShellBreakingChangeWarnings "true"
+
+try {
+  Import-Module Az
+
+  ##########################
+  # Azureへのログイン
+  ##########################
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] Azureへログイン:開始")
+  $LoginInfo = Login-AzAccount -Tenant $TennantID -WarningAction Ignore
+  if(-not $LoginInfo) { 
+    Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] Azureへログインできませんでした。")
+    exit 9
+  }
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] Azureへログイン:完了")
+
+  ###################################
+  # AzureVM 確認
+  ###################################
+  $AzureVMInfo = Get-AzVM -ResourceGroupName $AzureVMResourceGroupName -Name $AzureVMName
+  if(-not $AzureVMInfo) {
+    Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] " + $AzureVMResourceGroupName + "に" + $AzureVMName + "が存在しません。")
+    exit 9
+  }
+
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] リカバリポイントの選択:開始")
+  ##############################
+  # リストボックスフォーム作成
+  ##############################
+  Add-Type -AssemblyName System.Windows.Forms
+  Add-Type -AssemblyName System.Drawing
+
+  $form = New-Object System.Windows.Forms.Form
+  $form.Text = 'リカバリ対象を選択してください。'
+  $form.Size = New-Object System.Drawing.Size(350,200)
+  $form.StartPosition = 'CenterScreen'
+
+  $OKButton = New-Object System.Windows.Forms.Button
+  $OKButton.Location = New-Object System.Drawing.Point(75,120)
+  $OKButton.Size = New-Object System.Drawing.Size(75,23)
+  $OKButton.Text = 'OK'
+  $OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+  $form.AcceptButton = $OKButton
+  $form.Controls.Add($OKButton)
+
+  $CancelButton = New-Object System.Windows.Forms.Button
+  $CancelButton.Location = New-Object System.Drawing.Point(150,120)
+  $CancelButton.Size = New-Object System.Drawing.Size(75,23)
+  $CancelButton.Text = 'Cancel'
+  $CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+  $form.CancelButton = $CancelButton
+  $form.Controls.Add($CancelButton)
+
+  $label = New-Object System.Windows.Forms.Label
+  $label.Location = New-Object System.Drawing.Point(10,20)
+  $label.Size = New-Object System.Drawing.Size(320,20)
+  $label.Text = 'リカバリ対象を選択してください：'
+  $form.Controls.Add($label)
+
+  $listBox = New-Object System.Windows.Forms.ListBox
+  $listBox.Location = New-Object System.Drawing.Point(10,40)
+  $listBox.Size = New-Object System.Drawing.Size(310,20)
+  $listBox.Height = 80
+
+  ##############################
+  # Snapshot作成時間の一覧化
+  ##############################
+  Get-AzSnapshot -ResourceGroupName $AzureVMResourceGroupName | ? { $_.Tags.SourceVMName -ne $null -and $_.Tags.SourceVMName -eq $AzureVMName } | sort {$_.tags.CreateDate} -unique | % { [void] $listBox.Items.Add($_.tags.CreateDate) } 
+  $form.Controls.Add($listBox)
+  
+  ##############################
+  # リストボックスの表示
+  ##############################
+  $form.Topmost = $true
+  $result = $form.ShowDialog()
+
+  if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
+    $RecoveryPoint = $listBox.SelectedItem
+    Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] " + $RecoveryPoint + "にデータディスクを復元します")
+  } elseif ($result -eq [System.Windows.Forms.DialogResult]::Cancel) {
+    exit 0
+  } else {
+    exit 0
+  }
+
+  ##############################
+  # リカバリ対象ディスクの一覧
+  ##############################
+  $listBox.Items.Clear();  
+  $listBox.SelectionMode = 'MultiExtended'
+  Get-AzSnapshot -ResourceGroupName $AzureVMResourceGroupName | ? { $_.tags.CreateDate -ne $null -and $_.tags.CreateDate -eq $RecoveryPoint } | % { [void] $listBox.Items.Add($_.Name) } 
+  $form.Controls.Add($listBox)
+  
+  ##############################
+  # リストボックスの表示
+  ##############################
+  $form.Topmost = $true
+  $result = $form.ShowDialog()
+
+  if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
+    $SelectedSnapshots = $listBox.SelectedItems
+    $SelectedSnapshots | % { Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] " + $_ + "を復元します") }
+    Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] リカバリポイントの選択:完了")
+  } elseif ($result -eq [System.Windows.Forms.DialogResult]::Cancel) {
+    exit 0
+  } else {
+    exit 0
+  }
+
+  $RecoverySnapshots = $SelectedSnapshots | % { Get-AzSnapshot  -ResourceGroupName $AzureVMResourceGroupName -SnapshotName $_ }
+  foreach($Snapshot in $RecoverySnapshots) {
+    if($Snapshot.Tags.SourceDiskName -eq $null -or $Snapshot.Tags.SourceDiskName -eq "") {
+      Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] ディスクのSnapshotにディスクの復元情報（Tag）が無い為リカバリできません。")
+      exit 9
+    }
+  }
+
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] 仮想マシン(" + $AzureVMInfo.Name + ")の復元:開始")
+  ########################################
+  ## 仮想マシンの停止
+  ########################################
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] 仮想マシンの停止:開始")
+  $Result = Stop-AzVM -Name $AzureVMInfo.Name -ResourceGroupName $AzureVMInfo.ResourceGroupName -Force
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] 仮想マシンの停止:" + $Result.Status)
+  if($Result.Status -ne "Succeeded") {
+    Write-Output($Result | format-list -DisplayError)
+    exit 9
+  }
+
+
+  ########################################
+  ## データディスクのデタッチ
+  ########################################
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] データディスクのデタッチ:開始")
+  $Results = $RecoverySnapshots | % { Remove-AzVMDataDisk -VM $AzureVMInfo -Name $_.Tags.SourceDiskName }
+  foreach($Result in $Results) {
+    Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] データディスクのデタッチ:" + $Result.ProvisioningState)
+  }
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] データディスクのデタッチ:完了")
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] 仮想マシンの更新:開始")
+  $Result = Update-AzVM -ResourceGroupName $AzureVMInfo.ResourceGroupName -VM $AzureVMInfo
+  if(-not $Result -or $Result.IsSuccessStatusCode -ne "True") {
+    Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] 仮想マシンの更新:失敗")
+    exit 9
+  }
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] 仮想マシンの更新:完了")
+　
+  ########################################
+  ## データディスクの削除
+  ########################################
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] データディスク削除処理:開始")
+  $Results = $RecoverySnapshots | % { Remove-AzDisk -ResourceGroupName $AzureVMInfo.ResourceGroupName -DiskName $_.Tags.SourceDiskName -Force }
+  foreach($Result in $Results) {
+    Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] データディスクの削除:" + $Result.Status)
+  }
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] データディスク削除処理:完了")
+
+  #######################################################
+  ## 選択されたリカバリポイントのスナップショットを復元
+  #######################################################
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] データディスクの復元処理:開始")
+  $RecoveredDisks = $RecoverySnapshots | % { New-AzDiskConfig -Location $_.Location -SourceResourceId $_.Id -CreateOption Copy -Tag $_.Tags } | % { New-AzDisk -Disk $_ -ResourceGroupName $AzureVMResourceGroupName -DiskName $_.Tags.SourceDiskName } | % { Add-AzVMDataDisk -VM $AzureVMInfo -Name $_.Name -ManagedDiskId $_.Id -Lun $_.Tags.SourceLun -Caching ReadOnly -CreateOption Attach }
+  foreach($Result in $RecoveredDisks) {
+    Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] データディスクの復元:" + $Result.ProvisioningState)
+  }
+  $Result = Update-AzVM -ResourceGroupName $AzureVMInfo.ResourceGroupName -VM $AzureVMInfo
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] データディスクの復元処理:完了")
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] 仮想マシンの構成更新:開始")
+  if(-not $Result -or $Result.IsSuccessStatusCode -ne "True") {
+    Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] 仮想マシンの構成更新:失敗")
+    exit 9
+  }
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] 仮想マシンの構成更新:完了")
+  Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] 仮想マシン(" + $AzureVMInfo.Name + ")の復元:完了")
+
+  #################################################
+  # エラーハンドリング
+  #################################################
+  if($CreateVMJob.Status -eq "Failed") {
+    Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] 仮想マシンの復元処理中にがエラー終了しました。")
+    $CreateVMJob | Format-List -DisplayError
+    exit 9
+  } else {
+    Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] 仮想マシンの再構築処理:完了")
+  }
+} catch {
+    Write-Output("`r`n`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] 仮想マシンの復元処理中にエラーが発生しました。")
+    Write-Output("`[$(Get-Date -UFormat "%Y/%m/%d %H:%M:%S")`] " + $error[0] | Format-List --DisplayError)
+    exit 99
+}
+exit 0
