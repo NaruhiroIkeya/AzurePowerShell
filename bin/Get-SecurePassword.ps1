aa39ba901d50e5346d8f1d5d87f8a59666136af8
NaruhiroIkeya <51503619+NaruhiroIkeya@users.noreply.github.com>
2023-09-26 13:09:41 +0900

    20230926

---
+++ bin/Get-SecurePassword.ps1
@@ -0,0 +1,29 @@
+<################################################################################
+## Copyright(c) 2023 BeeX Inc. All rights reserved.
+## @auther#Naruhiro Ikeya
+##
+## @name: Get-SecurePassword.ps1
+## @summary: Create Encription Key and Secure Password
+##
+## @since:2023/08/04
+## @version:1.1
+## @see:
+## @parameter:Plain Password
+##
+## @return:0:Success 9:Error
+################################################################################>
+Param([parameter(mandatory=$true)] [string] $PlainPassword)
+
+
+$EncryptedKey = New-Object Byte[] 24
+
+[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($EncryptedKey)
+$KeyString = $EncryptedKey -join ","
+
+
+$SecureString = ConvertTo-SecureString -String $PlainPassword -AsPlainText -Force
+$EncryptedPassword = ConvertFrom-SecureString -SecureString $SecureString -key $EncryptedKey
+
+Write-Host "If your password string contains special characters, please escape them with backquote (``). ex.) `$`,`&```"`'"
+Write-host "<encryptedkey>$KeyString </encryptedkey>"
+Write-host "<encryptedpass>$EncryptedPassword</encryptedpass>"
